d9d1817e2ca52ad37e915b3e455fb64b0cc77bbe
sanu-programmer <sanuk8137@gmail.com>
2025-01-28 18:17:42 +0530

    javascript 2

---
+++ JavaScript/2_Lecture/script.js
@@ -0,0 +1,57 @@
+/*This is a code that  prints 
+ hello world in our console window*/
+ console.log("Hello world!");
+
+ //Arithematic operator 
+//  let a =5;
+//  let b =2;
+
+//  console.log("a+b =",a+b) //7
+//  console.log("a-b =",a-b) //3
+//  console.log("a*b =",a*b) //10
+//  console.log("a/b =",a/b) //2.5
+//  console.log("a%b =",a%b) //1
+//  console.log("a**b =",a**b) //25
+
+ //Unary operator
+//  console.log("a = ",a,"b = ", b);
+//  a++;//6
+//  console.log("a =", a); //6
+
+
+//  assignment operator 
+//  = += -= *= 
+
+// comparisioon operator 
+// equalto == , equalto and type  === 
+// not equal to != , not equal to and type !==
+
+// logical operator 
+// logical and  &&  - if cond 1 is true and cond 2 is true then only result will be true 
+// logical or II  - if any of the two operator is true then the result will be true and same thing goes with false
+// logical not ! - if the statement is true result will be false and if the statement is false the result will be true
+
+// conditional Statement 
+
+let mode = "light";
+let color;
+
+if(mode === "dark"){
+    color = "black";
+}
+
+if(mode === "light"){
+    color = "white";
+}
+console.log(color);
+
+let age = 16;
+if(age>18){
+    console.log("vote");
+}
+else{
+    console.log("not vote");
+}
+
+
+
