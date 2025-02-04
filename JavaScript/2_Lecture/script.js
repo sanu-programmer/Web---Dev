8811b4bb25234f79ce68a4ae77dd79330d60bfcf
sanu-programmer <sanuk8137@gmail.com>
2025-02-04 19:49:33 +0530

    Lecture 2!

--- JavaScript/2_Lecture/script.js
+++ JavaScript/2_Lecture/script.js
@@ -33,17 +33,17 @@
 
 // conditional Statement 
 
-let mode = "light";
-let color;
+// let mode = "light";
+// let color;
 
-if(mode === "dark"){
-    color = "black";
-}
+// if(mode === "dark"){
+//     color = "black";
+// }
 
-if(mode === "light"){
-    color = "white";
-}
-console.log(color);
+// if(mode === "light"){
+//     color = "white";
+// }
+// console.log(color);
 
 let age = 16;
 if(age>18){
@@ -52,6 +52,73 @@ if(age>18){
 else{
     console.log("not vote");
 }
+
+let num = 7;
+if(num %2 === 0){
+    console.log(num," is even");
+}
+else{
+    console.log(num,"is odd");
+}
+
+let mode = "blue";
+let color;
+ if(mode === "dark"){
+    color = "black";
+ }
+ else if(mode === "blue"){
+    color = "blue";
+ }
+ else if(mode ==="pink" ){
+    color="white";
+ }
+ else{
+    color = "white";
+ }
+
+ console.log(color);
+
+ if(mode === "dark") console.log(mode);
+
+//  Ternary Operators  simpler if else
+ let age1 = 16;
+ let result = age1>18 ? "adult" : "not adult";
+ console.log(result);
+
+ let age2 = 25; 
+ age2>18 ? console.log("adult") : console.log("not adult");
+
+alert("hello");
+let name = prompt("Hii Sanu ");
+console.log(name);
+
+let num1 = prompt("Enter a number: ");
+if(num1 % 5 === 0){
+    console.log(num1,"is multiple of 5");
+}
+else{
+    console.log(num1,"is not  multiple of 5");
+}
+
+let score = prompt("Enter the score:");
+let grade;
+if(score>=90 && score <= 100){
+    console.log(grade = "A");
+}
+else if(score >=70 && score <= 89){
+    console.log(grade = "B");
+}
+else if(score >=60 && score <= 69){
+    console.log(grade = "C");
+}
+else if(score >=50 && score <= 59){
+    console.log(grade = "D");
+}
+else if(score >=0 && score <= 49){
+    console.log(grade = "F");
+}
+
+
 
 
 
---
+++ JavaScript/2_Lecture/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+if(age>18){
+    console.log("vote");
+}
+else{
+    console.log("not vote");
+}
